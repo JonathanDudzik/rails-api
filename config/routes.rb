91d61327abbf197bb1f21a8ca6e7bc72b912336d
jonathan-dudzik <jdudzik@mavericksolutions.net>
2022-02-04 16:31:42 -0500

    clean up syntax in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the Domain Specific Language available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :books
   get '/books', to: 'books#index'
-  post 'books', to: 'books#create'
+  post '/books', to: 'books#create'
   get '/books/:id', to: 'books#show'
-  patch 'books/:id', to: 'books#update'
-  put 'books/:id', to: 'books#update'
-  delete 'books/:id', to: 'books#destroy'
+  patch '/books/:id', to: 'books#update'
+  put '/books/:id', to: 'books#update'
+  delete '/books/:id', to: 'books#destroy'
 end
