fafd5dfd8c48d660dcba746cecae9f7d7f03aa8b
jonathan-dudzik <jdudzik@mavericksolutions.net>
2022-02-05 15:58:38 -0500

    make the books index method the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the Domain Specific Language available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :books
+  root 'books#index'
   get '/books', to: 'books#index'
   post '/books', to: 'books#create'
   get '/books/:id', to: 'books#show'
